714969894496aa0122f067845736b740996cac60
Ravel Mesquita <ravels78@gmail.com>
2024-10-06 14:18:18 -0300

    Codigo de conferencia de palavras

--- codigo.js
+++ codigo.js
@@ -1,11 +1,17 @@
 const text = document.getElementById("box");
 const quantidadeCaracteres = document.getElementById("caracters")
 
-// captura e conferencia de caracteres encritos no input
+// captura e conferencia de caracteres escritos no input
 text.addEventListener('input', function(){
+    //quantidade de caracteres
     let caracteres = text.value.length;
-    
-    
 
-    quantidadeCaracteres.innerText = `Quantidade de caracteres  ${caracteres}`;
+    //quantidade de palavras
+    let palavras = text.value.trim().split(/\s+/);
+    const count = palavras.length;
+    
+  
+
+
+    
 })
