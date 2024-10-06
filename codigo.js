28ec075857e7365028e1ef6a37cc1b00029a221d
Ravel Mesquita <ravels78@gmail.com>
2024-10-06 14:50:33 -0300

    Concluindo a logica de contagem

--- codigo.js
+++ codigo.js
@@ -1,17 +1,25 @@
 const text = document.getElementById("box");
-const quantidadeCaracteres = document.getElementById("caracters")
+const quantidadeCaracteres = document.getElementById("caracters");
 
 // captura e conferencia de caracteres escritos no input
-text.addEventListener('input', function(){
-    //quantidade de caracteres
-    let caracteres = text.value.length;
+text.addEventListener("input", function () {
+  //quantidade de caracteres
+  const caracteres = text.value.length;
 
-    //quantidade de palavras
-    let palavras = text.value.trim().split(/\s+/);
-    const count = palavras.length;
-    
+  //quantidade de palavras
+  //remove caracteres especiais
+  const textolimpo = text.value.replace(/[^\w\s]|[0-9]/g, '');
+  //separa em array as palavras
+  const palavras = textolimpo.trim().split(/\s+/);
+  // verifica e conta as quantidades de palavras
+  const count = textolimpo.trim() === "" ? 0 : palavras.length;
+//atualiza a quantidade na tela
+  quantidadeCaracteres.textContent = `Caracteres: ${caracteres} Palavaras: ${count}`;
   
+});
 
-
-    
-})
+//fução para limpar o conteudo da caixa de texto
+function apagar(){
+    text.value = "";
+    quantidadeCaracteres.textContent = "Caracteres: 0 Palavaras: 0";
+}
